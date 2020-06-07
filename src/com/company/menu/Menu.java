fec426fac964291b72d1b52acdd3bada6052d7fc
gnosinporta <56847249+gnosinporta@users.noreply.github.com>
2020-06-07 20:26:14 -0300

    Add files via upload

--- src/com/company/menu/Menu.java
+++ src/com/company/menu/Menu.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Menu {
 
-    private final String titulo = "BIENVENIDO A NUESTRA EMPRESA DE ALQUILER DE AEROTAXIS";
+    private final String TITULO = "BIENVENIDO A NUESTRA EMPRESA DE ALQUILER DE AEROTAXIS";
 
     //constructor vacio
     public void Menu(){}
@@ -13,20 +13,21 @@ public class Menu {
 
         Scanner sc = new Scanner(System.in);
         boolean flag = true;
+        private int opcion;
 
         while(flag == true) {
 
-            System.out.print(titulo + "\n\n" +
+            System.out.print(TITULO + "\n\n" +
                     "1 - Acceso de Usuarios\n" +
                     "2 - Registro de Nuevo Usuario\n" +
                     "0 - Salir\n\n" +
                     "elija una opcion: ");
 
-            int opcion = sc.nextInt();
+            opcion = sc.nextInt();
 
             switch (opcion) {
                 case 1:
-                    //accesoUsuario();
+                    accesoUsuario();
                     break;
                 case 2:
                     //registroUsuario();
@@ -37,6 +38,29 @@ public class Menu {
                     break;
                 default:
                     System.out.println("\nopción incorrecta, por favor intente de nuevo\n");
+            }
+        }
+    }
+
+    public void accesoUsuario(){
+        Scanner sc = new Scanner(System.in);
+        private boolean flag = true;
+        private String user;
+        private String pass;
+        private boolean userValid;
+
+        while(flag == true){
+            System.out.print("ACCESO DE USUARIOS\n\n" +
+                    "usuario: ");
+            user = sc.nextLine();
+
+            /*LLAMADA A VALIDACIÓN DE USUARIO
+            * userValid = validarUsuario(user) */
+            userValid = true;   //linea provisoria - borrar cuando se implemente el método de control
+
+            if(userValid){
+                System.out.print("\npass: ");
+                pass = sc.nextLine();
             }
         }
     }
