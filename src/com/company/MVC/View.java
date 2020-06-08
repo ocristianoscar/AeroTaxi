4c6f8c4f964529176d4a998ba5bb129d08adb49b
Agustin <agus.sanmartin95@gmail.com>
2020-06-08 16:24:45 -0300

    MVC Added, working on Vuelos & Acompañantes

--- src/com/company/MVC/View.java
+++ src/com/company/MVC/View.java
@@ -1,4 +1,5 @@
 package com.company.MVC;
 
 public interface View {
+    void modelChanged();
 }
--- src/com/company/Model/Vuelos.java
+++ src/com/company/Model/Vuelos.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Vuelo {
-    private List<Vuelo> visits = new ArrayList<>();
+public class Vuelos {
+    private List<Vuelo> vuelos = new ArrayList<>();
 
     public void add(Vuelo vuelo) {
-        this.visits.add(vuelo);
+        this.vuelos.add(vuelo);
     }
 
     public List<Vuelo> on(LocalDate date) {
-        return visits.stream()
-                .filter(visit -> visit.isOn(date))
+        return vuelos.stream()
+                .filter(vuelo -> vuelo.isOn(date))
                 .collect(Collectors.toList());
     }
 }
