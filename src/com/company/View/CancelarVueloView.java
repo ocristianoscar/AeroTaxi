e765f508a54f760c860ccd44f81458c9c4e6ffa1
Agustin <agus.sanmartin95@gmail.com>
2020-06-09 22:20:42 -0300

    basics of MVC completely added

--- src/com/company/View/CancelarVueloView.java
+++ src/com/company/View/CancelarVueloView.java
@@ -1,4 +1,29 @@
 package com.company.View;
 
-public class CancelarVueloView {
+import com.company.App.Admin;
+import com.company.App.CancelarVuelo;
+import com.company.Controller.AdminController;
+import com.company.Controller.CancelarVueloController;
+import com.company.MVC.View;
+
+import java.time.DayOfWeek;
+
+public class CancelarVueloView implements View {
+
+    private CancelarVuelo cancelarVuelo;
+    private CancelarVueloController cancelarVueloController;
+
+    public CancelarVueloView(CancelarVuelo cancelarVuelo, CancelarVueloController cancelarVueloController) {
+        this.cancelarVuelo = cancelarVuelo;
+        this.cancelarVueloController = cancelarVueloController;
+        cancelarVuelo.addView(this);
+    }
+
+    public void modelChanged() {
+        show();
+    }
+
+    public void show() {
+
+    }
 }
