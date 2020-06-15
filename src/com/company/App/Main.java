7b767b02a6c05fcf5b4284d3cc88143f0a9cfd88
Agustin <agus.sanmartin95@gmail.com>
2020-06-15 06:32:21 -0300

    Register MVC added

--- src/com/company/App/Main.java
+++ src/com/company/App/Main.java
@@ -1,13 +1,20 @@
 package com.company.App;
 
+import com.company.Controller.MainMenuController;
+import com.company.Domain.Fecha;
 import com.company.Model.Bronze;
 import com.company.Model.Gold;
 import com.company.Model.Propulsion;
 import com.company.Model.Silver;
+import com.company.View.MainMenuView;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static void main(String[] args) {
+        Fecha fechaActual = Fecha.desde(LocalDate.now());
         Bronze bronce1 = new Bronze(9500,150,10,1050,Propulsion.REACCION,false);//Dassault Falcon 900LX
         Bronze bronce2 = new Bronze(9500,150,10,1050,Propulsion.REACCION,false);
         Bronze bronce3 = new Bronze(7500,175,19,1050,Propulsion.REACCION,false);//Dassault Falcon 2000LX
@@ -21,6 +28,9 @@ public class Main {
         Gold gold2 = new Gold(6000,300,12,1150, Propulsion.REACCION,true,false);
         Gold gold3 = new Gold(6000,300,12,1150, Propulsion.REACCION,true,false);
 
-
+        MainMenu mainMenu = new MainMenu(fechaActual);
+        MainMenuController mainMenuController = new MainMenuController(mainMenu);
+        MainMenuView mainMenuView = new MainMenuView(mainMenu, mainMenuController);
+        mainMenuView.show();
     }
 }
--- src/com/company/Controller/RegistrarController.java
+++ src/com/company/Controller/RegistrarController.java
@@ -3,11 +3,20 @@ package com.company.Controller;
 import com.company.App.CancelarVuelo;
 import com.company.App.Registrar;
 import com.company.MVC.Controller;
+import com.company.Model.Usuario;
+
+import java.util.ArrayList;
 
 public class RegistrarController implements Controller {
 
     private Registrar registrar;
-
+    private String nombre;
+    private String apellido;
+    private String DNI;
+    private String edad;
+    private String user;
+    private String password;
+    private ArrayList<String> usuarioAcrear = new ArrayList();
     public RegistrarController(Registrar registrar) {
         this.registrar = registrar;
     }
@@ -15,22 +24,22 @@ public class RegistrarController implements Controller {
     public boolean execute(String command) {
         switch (command){
             case "1":
-                //TODO:
+                this.nombre = usuarioAcrear.get(0);
+                this.apellido = usuarioAcrear.get(1);
+                this.DNI = usuarioAcrear.get(2);
+                this.edad = usuarioAcrear.get(3);
+                this.user = usuarioAcrear.get(4);
+                this.password = usuarioAcrear.get(5);
+                //AGREGAR AL JSON EL USUARIO REGISTRADO Y LEER EN MainMenuView
                 return true;
             case "2":
-                //TODO:
-                return true;
-            case "3":
-                //TODO:
-                return true;
-            case "4":
-                //TODO:
-                return true;
-            case "5":
                 //TODO:
                 return true;
             default: break;
         }
         return false;
     }
+
+    public ArrayList getUsuarioAcrear() { return usuarioAcrear; }
+
 }
