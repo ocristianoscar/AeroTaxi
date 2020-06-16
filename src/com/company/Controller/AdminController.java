6b5e44b1bf732ec856cc1ed324babf8a8f2c4e7d
Gnosin <gnosin.porta@gmail.com>
2020-06-16 16:32:04 -0300

    some edits in messages from registrar process, and some changes in Admin

--- src/com/company/Controller/AdminController.java
+++ src/com/company/Controller/AdminController.java
@@ -16,10 +16,29 @@ public class AdminController implements Controller {
     public boolean execute(String command) {
         switch (command){
             case "1":
-                //TODO administracion de aviones
+                //TODO lista de todos los vuelos programados para una fecha dada
+
+                System.out.print("\ningrese fecha (en formato aaaa-mm-dd): ");
+                //aqui también se podría hacer 3 inputs diferentes, uno para cada dato
+
+                //validar fecha
+
+                //recorrer la lista de vuelos y levantar los de la fecha elegida
+                //o mostrar "no hay vuelos en la fecha elegida"
+
+                //metodo "mostrarVuelo(Vuelo vuelo)"
+
                 return true;
             case "2":
-                //TODO salidas del sistema
+                //TODO lista de clientes, indicando datos personales, categoría más alta de vuelo contratada
+                // y total de gastos en vuelos
+
+                /*si se muestran todos los clientes ordenados por apellido habría que ordenarlos quizá
+                antes de subirlos a la lista. Si se los muestra ordenados por id, directamente se los lee
+                como están en la lista*/
+
+                //metodo "mostrarCliente(Usuario usuario)"
+
                 return true;
             default: break;
         }
